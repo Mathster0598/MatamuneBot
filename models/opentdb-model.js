77ecec1264892d1d8eb9345ba47bb0298e880a68
mathster0598 <24941318+Mathster0598@users.noreply.github.com>
2020-10-30 20:38:31 +0800

    update saved

--- models/opentdb-model.js
+++ models/opentdb-model.js
@@ -2,11 +2,21 @@ const Sequelize = require('sequelize');
 const db = require('../utility/database.js');
 
 module.exports = {
-	async sessionToken() {
-		const token = await db.define('opentdb_tokens', {
+	async sessions() {
+		const tokens = await db.define('opentdb_tokens', {
+			guild: Sequelize.STRING,
 			token: Sequelize.STRING,
 		});
 
-		return token;
+		return tokens;
+	},
+	async categories() {
+		const categories = await db.define('opentdb_categories', {
+			guild: Sequelize.STRING,
+			categoryId: Sequelize.STRING,
+			categoryName: Sequelize.STRING,
+		});
+
+		return categories;
 	},
 };
--- utility/opentbd.js
+++ utility/opentbd.js
@@ -1,6 +1,12 @@
 const fetch = require('node-fetch');
-const { sessionToken } = require('../models/opentdb-model.js');
+const { sessions, categories } = require('../models/opentdb-model.js');
 const { trivia } = require('../config.json');
+
+const Token = sessions();
+Token.sync();
+
+const Category = categories();
+Category.sync();
 
 const dbURL = trivia.source;
 
@@ -9,24 +15,48 @@ const codes = ['Success', 'No results', 'Invalid parameter', 'Token not found', 
 module.exports = {
 	async getToken() {
 		const result = await fetch(`${dbURL}/api_token.php?command=request`);
-		if(result !== 0) return console.log(`Received code: ${codes[result.response_code]} while trying to get token.`);
+		if (result !== 0) return console.log(`Received code: ${codes[result.response_code]} while trying to get token.`);
 		else console.log(`Success! Token: ${result.token}`);
 		return result.token;
 	},
 	async resetToken(token) {
 		const result = await fetch(`${dbURL}/api_token.php?command=reset&token=${token}`);
-		if(result !== 0) return console.log(`Received code: ${codes[result.response_code]} while trying to get token.`);
+		if (result !== 0) return console.log(`Received code: ${codes[result.response_code]} while trying to get token.`);
 		else console.log(`Success! Token: ${result.token}`);
 		return result.token;
 	},
-	// async checkToken() {
-	// 	const checkToken = await Vanity.findAll().catch(console.error);
-	// 	if(!checkToken) {
-
-	// 	}
-	// }
-	// async getQuestions(number) {
-
-	// },
+	async checkToken(guild) {
+		const checkToken = await Token.findAll().catch(console.error);
+		if (checkToken) { return checkToken; }
+		else {
+			const token = this.getToken();
+			const session_token = {
+				values: {
+					token: token,
+					guild: guild,
+				},
+				condition: {
+					where: { guild: guild },
+				},
+			};
+			const updateToken = await Token.update(session_token.values, session_token.condition);
+			if (!updateToken) console.log('Database was not updated');
+			else return updateToken;
+		}
+	},
+	async saveCategories(guild) {
+		const result = await fetch(`${dbURL}/api_category.php`);
+		if (result) { return console.log('No categories found!'); }
+		else {
+			result.trivia_categories.array.forEach(async item => {
+				await Category.create({
+					guild: guild,
+					categoryId: item.id.toString(),
+					categoryName: item.name,
+				}).catch(console.error);
+			});
+		}
+		return 'success';
+	},
 };
 
